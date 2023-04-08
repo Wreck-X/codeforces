174349f257ef2e57abbea1ddc1f5e31aa7567c2c
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-04-08 23:57:14 +0530

    fixes to code

--- summerselloff.py
+++ summerselloff.py
@@ -1,10 +1,13 @@
 n,y = map(int,input().split())
-
 dict = {}
 remlist = []
 subdict = {}
+itemarray = []
+personarray = []
 for i in range(n):
     array = [*map(int,input().split())]
+    itemarray.append(array[0])
+    personarray.append(array[1])
     dict[i] = array
     if y != 0:
         if array[0] < array[1] and array[0]*2 > array[1]:
@@ -19,7 +22,17 @@ for i in subdict:
     taken.append(subdict[i][1])
 
 taken.sort()
+itemarray.sort(reverse=True)
+personarray.sort(reverse=True)
 result = taken[-y::]
+if y != 0 and result == []:
+    for i in range(y):
+        if itemarray[i] < personarray[i]:
+            result.append(itemarray[i])
+        if itemarray[i] > personarray[i]:
+            pass
+
+
 for i in dict:
     if dict[i][0] > dict[i][1]:
         result.append(dict[i][1])
